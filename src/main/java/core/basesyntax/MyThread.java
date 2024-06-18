99e37b4cb39645dc2268575c632eac7f2be4f053
Oksana Miazina <oksana.myazina@gmail.com>
2024-06-18 18:35:23 +0300

    feat: implement MyThread

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime start = LocalTime.now();
+        Thread.sleep(new Random().nextInt(1000));
+        LocalTime end = LocalTime.now();
+        long duration = ChronoUnit.MILLIS.between(start, end);
+        return "Task duration was %d ms, execution finished at %s"
+                .formatted(duration, end);
     }
 }
